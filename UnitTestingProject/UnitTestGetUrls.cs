ebd38b71da9d6d835c8758d686eb48c3749907e1
Ishaan-Laroia <77766970+Ishaan-Laroia@users.noreply.github.com>
2023-02-12 03:24:11 -0500

    Created GetUrlsTestingClass

---
+++ UnitTestingProject/UnitTestGetUrls.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ECE461Project1;
+
+namespace UnitTestingProject
+{
+    [TestClass]
+    public class UnitTestGetUrls
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
