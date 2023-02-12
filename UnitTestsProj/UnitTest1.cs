67b51ead082448b714765ce3cb8b025c3a5395af
Ishaan-Laroia <77766970+Ishaan-Laroia@users.noreply.github.com>
2023-02-12 04:27:58 -0500

    Updated Dependencies

---
+++ UnitTestsProj/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ECE461Project1;
+
+namespace UnitTestsProj
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void License()
+        {
+            string url = "https://github.com/nodejs/node-addon-api";
+            License license = new License();
+            float result = license.GetScore(url);
+            Assert.AreEqual(0, result, "Incorrect Result");
+        }
+    }
+}
