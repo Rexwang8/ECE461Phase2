70139c82040f2235a73693f2d2384be6b88cb087
Ishaan-Laroia <77766970+Ishaan-Laroia@users.noreply.github.com>
2023-02-11 20:29:34 -0500

    added unit testing project

---
+++ UnitTestingProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ECE461Project1;
+
+namespace UnitTestingProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
