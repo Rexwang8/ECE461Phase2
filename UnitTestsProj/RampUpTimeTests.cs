4d17f1f7cb1cb782276ca57cdaf9a4436d14e5a0
Ishaan-Laroia <77766970+Ishaan-Laroia@users.noreply.github.com>
2023-02-12 16:01:54 -0500

    Ramp Up Time Unit Tests

---
+++ UnitTestsProj/RampUpTimeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ECE461Project1;
+using System.Collections.Generic;
+
+namespace UnitTestsProj
+{
+    [TestClass]
+    public class RampUpTimeTests
+    {
+        [TestMethod]
+        public void GitUrlTest()
+        {
+            string url = "https://github.com/nodejs/node-addon-api";
+            RampUpTime rampUpTime = new RampUpTime();
+            float result = rampUpTime.GetScore(url); ;
+            float expectedValue = 0.47025f;
+            float acceptableDeviation = 0.01f;
+            Assert.IsTrue((expectedValue - acceptableDeviation <= result) && (result <= expectedValue + acceptableDeviation));
+        }
+    }
+}
