e1003f4a66bfc37cf3bb065e0a46b65a0e9231ac
5OL4M3 <klin0732@gmail.com>
2023-03-12 13:33:36 -0400

    bus factor completed

--- CLI/src/StaticAnalysis/src/BusFactor.cs
+++ CLI/src/StaticAnalysis/src/BusFactor.cs
@@ -1,40 +1,82 @@
 namespace StaticAnalysis;
 public static class BusFactor
 {
-    /*
-    BusFactor
-
-    Metric   Weight
-    ------------------
-    Issues      30%
-    Health      70%
-    Forks       
-
-
-    */
     public static float GetScore(URLInfo urlInfo)
     {   
         Console.WriteLine("-----\nCalculating BusFactor");
 
-        float finalScore = 0;
-        const float ISSUE_WEIGHT = .3f;
-        const float HEALTH_WEIGHT = .7f;
+        float ISSUE_WEIGHT = 0;
+        float HEALTH_WEIGHT = 0;
+        float FORK_WEIGHT = 0;
+        float STATE_WEIGHT = 0;
+        float DISCUSSION_WEIGHT = 0;
+        float WATCHER_WEIGHT = 0;
 
+        float finalScore = 0;
         int health_score = 0;
         int fork_score = 0;
+        int state_score = 0;
+        int discussion_score = 0;
+        int watcher_score = 0;
 
-        //Calculate Issues
-        if(urlInfo.githubIssues != 0 && urlInfo.githubOpenIssues != 0)
+        //Check Repo State
+        if (urlInfo.githubIsArchived || urlInfo.githubIsDisabled || urlInfo.githubIsEmpty || urlInfo.githubIsLocked)
         {
-            finalScore += ISSUE_WEIGHT * (urlInfo.githubIssues - urlInfo.githubOpenIssues)/urlInfo.githubIssues;
-            Console.WriteLine($"There are {urlInfo.githubIssues} githubIssues");
-            Console.WriteLine($"There are {urlInfo.githubOpenIssues} githubOpenIssues");
+            ISSUE_WEIGHT = .20f;
+            HEALTH_WEIGHT = .05f;
+            FORK_WEIGHT = .22f;
+            STATE_WEIGHT = .3f;
+            DISCUSSION_WEIGHT = .1f;
+            WATCHER_WEIGHT = .13f;
+
+            state_score += 0;  
         }
         else
         {
-            Console.WriteLine("No Issues");
+            ISSUE_WEIGHT = .10f;
+            HEALTH_WEIGHT = .05f;
+            FORK_WEIGHT = .35f;
+            STATE_WEIGHT = .05f;
+            DISCUSSION_WEIGHT = .25f;
+            WATCHER_WEIGHT = .2f;
+
+            state_score += 1;
+        }
+        finalScore += STATE_WEIGHT * state_score;
+        
+        //Calculate Issues
+        if(urlInfo.githubIssues != 0)
+        {
+            finalScore += ISSUE_WEIGHT * (urlInfo.githubIssues - urlInfo.githubOpenIssues)/urlInfo.githubIssues;
+        }
+        else
+        {
+            finalScore += ISSUE_WEIGHT * 1;
         }
         
+        //Check if Discussions
+        if (urlInfo.githubDiscussions == 0)
+        {
+            discussion_score += 0;
+        }
+        else if (urlInfo.githubDiscussions <= 5)
+        {
+            discussion_score += 5;
+        }
+        else if (urlInfo.githubDiscussions <= 15)
+        {
+            discussion_score += 10;
+        }
+        else if (urlInfo.githubDiscussions <= 25)
+        {
+            discussion_score += 15;
+        }
+        else
+        {
+            discussion_score += 20;
+        }
+        finalScore += discussion_score * discussion_score/20;
+
         //Health Percentage: ReadME or License presence
         if (urlInfo.licensePath != "")
         {
@@ -47,32 +89,50 @@ public static class BusFactor
         finalScore += HEALTH_WEIGHT * health_score/2;
 
         //Forks
-        /* if (urlInfo.githubForks == 0)
+        if (urlInfo.githubForks == 0)
         {
-            //Check if there is any open issues 
-            
+            fork_score += 5;
         }
         else if (urlInfo.githubForks <= 25)
         {
-            
+            fork_score += 8;
         }
         else if (urlInfo.githubForks <= 50)
         {
-
+            fork_score += 12;
         }
         else if (urlInfo.githubForks <= 100)
         {
-
+            fork_score += 15;
         }
         else if (urlInfo.githubForks <= 500)
         {
-
+            fork_score += 18;
         }
         else
         {
-
-        } */
-
+            fork_score += 20;
+        }
+        finalScore += fork_score/20 * FORK_WEIGHT;
+        
+        //Watchers
+        if (urlInfo.githubWatchers == 0)
+        {
+            watcher_score += 0;
+        }
+        else if (urlInfo.githubWatchers <= 25)
+        {
+            watcher_score += 5;
+        }
+        else if (urlInfo.githubWatchers <= 50)
+        {
+            watcher_score += 8;
+        }
+        else
+        {
+            watcher_score += 10;
+        }
+        finalScore += watcher_score/10 * WATCHER_WEIGHT;
 
         return finalScore;
     }
